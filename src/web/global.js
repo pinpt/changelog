a998dd12a9e5f6fe2707bf7998e0c5c0e5aee491
Chris Bowley <cbowley@pinpoint.com>
2021-07-02 16:54:47 +0100

    Tidy up styles; fix highfive fill color

--- src/web/global.js
+++ src/web/global.js
@@ -133,9 +133,21 @@ function hydrateIndexTags() {
 
 function createTag(tag, background, color, border, remove) {
   const element = document.createElement('span');
-  element.classList = `tag clickable ${tag}`;
-  element.style = `background-color:${background};color:${color};border:${border}`;
-  element.innerHTML = `${remove && '×&nbsp;&nbsp;' || ''}${tag}`;
+  element.classList = `tag clickable tag-${tag}`;
+  element.style = `background-color:${background};color:${color};`;
+  // element.innerHTML = `${tag}${remove && '&nbsp;&nbsp;x' || ''}`;
+
+  const text = document.createElement('span');
+  text.innerHTML = tag;
+  element.appendChild(text);
+
+  if (remove) {
+    const icon = document.createElement('span');
+    icon.classList = 'icon';
+    icon.innerHTML = `<svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="times" class="svg-inline--fa fa-times fa-w-10" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M207.6 256l107.72-107.72c6.23-6.23 6.23-16.34 0-22.58l-25.03-25.03c-6.23-6.23-16.34-6.23-22.58 0L160 208.4 52.28 100.68c-6.23-6.23-16.34-6.23-22.58 0L4.68 125.7c-6.23 6.23-6.23 16.34 0 22.58L112.4 256 4.68 363.72c-6.23 6.23-6.23 16.34 0 22.58l25.03 25.03c6.23 6.23 16.34 6.23 22.58 0L160 303.6l107.72 107.72c6.23 6.23 16.34 6.23 22.58 0l25.03-25.03c6.23-6.23 6.23-16.34 0-22.58L207.6 256z"></path></svg>`;
+    element.appendChild(icon);
+  }
+
   return element;
 }
 
