8e63d88803c6c36bf4738fc2f38a59d483b17d79
Chris Bowley <cbowley@pinpoint.com>
2021-07-06 17:13:19 +0100

    Update default theme (#23)
    * Update default theme for greater flexibility; style search; consistent structure and naming
    
    * Tidy up styles; fix highfive fill color
    
    * A bit more tidy up; less purple, more indigo; fix up coming soon page
    
    * Start moving to css variables
    
    * More CSS variables; handle toggle node icon background; move clap svg into a partial
    
    * Tidy up
    
    * Use gray for default dark theme
    
    * Make theme switcher behave better in themes; smaller padding on small screens
    
    * Make title a heading; tweak margins
    
    * 2.0.3
    
    * Use tile title color
    
    * Remove mail address

--- src/web/global.js
+++ src/web/global.js
@@ -139,30 +139,17 @@
 
   function createTag(tag, background, color, border, remove) {
     const element = document.createElement("span");
-    element.classList = `tag clickable ${tag}`;
-    element.style = `background-color:${background};color:${color};border:${border}`;
-    element.innerHTML = `${(remove && "×&nbsp;&nbsp;") || ""}${tag}`;
-    return element;
-  }
-
-  function createSearchEmptyState() {
-    const element = document.createElement("div");
-    const container = document.createElement("p");
-    const clearFilters = document.createElement("div");
-
-    clearFilters.innerHTML = "×&nbsp;&nbsp;Remove Filters";
-    clearFilters.onclick = (e) => {
-      e.preventDefault();
-      e.stopPropagation();
-      exitFiltering();
-      return false;
-    };
-    clearFilters.classList.add("clear-button");
-    container.innerText = "No results matched your filters";
-    element.appendChild(container);
-    element.appendChild(clearFilters);
-    element.classList.add("empty-search");
-
+    element.classList = `tag clickable tag-${tag}`;
+    element.style = `background-color:${background};color:${color}`;
+    const text = document.createElement('span');
+    text.innerHTML = tag;
+    element.appendChild(text);
+    if (remove) {
+      const icon = document.createElement('span');
+      icon.classList = 'icon';
+      icon.innerHTML = `<svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="times" class="svg-inline--fa fa-times fa-w-10" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><path fill="currentColor" d="M207.6 256l107.72-107.72c6.23-6.23 6.23-16.34 0-22.58l-25.03-25.03c-6.23-6.23-16.34-6.23-22.58 0L160 208.4 52.28 100.68c-6.23-6.23-16.34-6.23-22.58 0L4.68 125.7c-6.23 6.23-6.23 16.34 0 22.58L112.4 256 4.68 363.72c-6.23 6.23-6.23 16.34 0 22.58l25.03 25.03c6.23 6.23 16.34 6.23 22.58 0L160 303.6l107.72 107.72c6.23 6.23 16.34 6.23 22.58 0l25.03-25.03c6.23-6.23 6.23-16.34 0-22.58L207.6 256z"></path></svg>`;
+      element.appendChild(icon);
+    }
     return element;
   }
 
@@ -380,12 +367,12 @@
     const filters = getFilters();
     const term = getSearchTerm();
 
-    const input = document.querySelector(".searchInput");
+    const input = document.querySelector('#search-input');
     const container = document.querySelector(
       "[data-changelog-id=__PLACEHOLDER_ID__]"
     );
-    const grid = document.querySelector(".grid");
-    const filterList = document.querySelector(".filters > .taglist");
+    const grid = document.querySelector(".tiles");
+    const filterList = document.querySelector("#filter-taglist");
 
     function renderRemoveSearchTermButton(value) {
       const element = createTag(
@@ -504,10 +491,7 @@
             res.hits.forEach((hit) =>
               grid.appendChild(getTileElement(hit, template))
             );
-          } else {
-            document.querySelector(".tiles").remove();
-            const element = createSearchEmptyState();
-            document.querySelector(".results").appendChild(element);
+            document.querySelector('#no-search-results').remove();
           }
         }
       }
@@ -527,9 +511,9 @@
   }
 
   // Wire up search
-  const form = document.querySelector(".searchForm");
+  const form = document.querySelector(".search-form");
   if (form) {
-    const input = document.querySelector(".searchInput");
+    const input = document.querySelector(".search-input");
     if (input) {
       form.onsubmit = (e) => {
         e.preventDefault();
@@ -544,3 +528,8 @@
     }
   }
 })();
+
+// remove preload class from body on load
+window.onload = function() {
+  document.querySelector('body').classList.remove('preload');
+};
